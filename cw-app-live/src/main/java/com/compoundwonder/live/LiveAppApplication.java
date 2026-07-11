988e64203c1b664003787f06df84fea5e0d80e31
chaobin <guochaobin@126.com>
2026-07-11 18:26:14 +0800

    项目初版完成

--- cw-app-live/src/main/java/com/compoundwonder/live/LiveAppApplication.java
+++ cw-app-live/src/main/java/com/compoundwonder/live/LiveAppApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableScheduling
+@SpringBootApplication(
+        scanBasePackages = "com.compoundwonder"
+)
 public class LiveAppApplication {
 
     public static void main(String[] args) {
